973ad4d94cf60638775b03292d8c14cff6de7551
mbsaloka <mbsaloka@gmail.com>
2024-10-13 23:57:03 +0700

    feat: add sample method usage in Main

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         InventoryController controller = new InventoryController(inventory);
         InventoryView view = new InventoryView(controller);
         view.setVisible(true);
+
+        inventory.tambahItem("Indomie Goreng", 10);
+        inventory.tambahItem("Indomie Soto", 5);
+        inventory.tambahItem("Indomie Ayam Bawang", 3);
+        inventory.hapusItem("Indomie Soto");
+        inventory.tampilkanItem();
     }
 }
